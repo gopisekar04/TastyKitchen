ae25a306c443b68665b2c34442cb07e6670769cd
gopi_0104 <itsgopiofficial@gmail.com>
2024-06-26 17:06:47 +0000

    added Header component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,42 @@
+/* eslint-disable camelcase */
 import './index.css'
+import {useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
 
 export default function Home() {
-  return <h1>Home</h1>
+  const history = useHistory()
+
+  useEffect(() => {
+    const jwt_token = Cookies.get('jwt_token')
+    if (!jwt_token) {
+      history.replace('/login')
+    }
+  })
+
+  useEffect(() => {
+    const fetchDeals = async () => {
+      const url = 'https://apis.ccbp.in/restaurants-list/offers'
+
+      const res = await fetch(url, {
+        method: 'GET',
+        'Content-Type': 'application/json',
+        headers: {
+          Authorization: `Bearer ${Cookies.get('jwt_token')}`,
+        },
+      })
+      const jsonData = await res.json()
+      console.log(jsonData)
+    }
+
+    // fetchDeals()
+  })
+
+  return (
+    <div>
+      <Header />
+      <h1>Home</h1>
+    </div>
+  )
 }
